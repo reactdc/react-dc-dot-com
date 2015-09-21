bb79f66553754849fb28acf11f3bb8b11ae0af3a
Ricky Vetter <ricky@socialtables.com>
2015-09-21 19:54:35 -0400

    setup

---
+++ webpack.config.babel.js
@@ -0,0 +1,23 @@
+import path from 'path';
+
+export default {
+    devtool: 'cheap-module-eval-source-map',
+    entry: [
+        './src/app/index.js'
+    ],
+    output: {
+        path: path.join(__dirname, 'public'),
+        filename: 'bundle.js'
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.js/,
+                exclude: /node_modules/,
+                loaders: [
+                    'babel-loader?{"stage":0,"plugins":["object-assign"]}'
+                ]
+            }
+        ]
+    }
+};
